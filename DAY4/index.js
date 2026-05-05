07edccf3bfcfa5e7317158b9d1b4f3036ece413d
stuti <singhstuti003@gmail.com>
2026-05-05 22:32:48 +0530

    promises and api calling and promising chain

--- DAY4/index.js
+++ DAY4/index.js
@@ -54,3 +54,39 @@ fetch("https://jsonplaceholder.typicode.com/todos")
 // setInterval(() => {
 //     console.log("Checking for new data...");
 // }, 3000)
+
+function getData() {
+    return new Promise((resolve, reject) => {
+        if (2 > 3) {
+            setTimeout(() => {
+                resolve("Data received");
+            }, 2000);
+        } else {
+            reject("not working");
+        }
+    });
+}
+
+//Fetch API Chain
+fetch("https://jsonplaceholder.typicode.com/todos")
+.then(res => res.json()) //covert response to json
+.then(data => data.filter(d => d.id % 2 === 0)) // filter even ids
+.then(data => data.map(d =>(d.title = "Prateek" + d.title))) // mpdify
+.then(data => console.log(data)) //print result
+.catch(error => console.error("Error fetching data :", error));
+
+
+// //Calling getData()
+
+// getData() 
+//      .then(data => {
+//           console.log("getData success :", data);
+//      })
+//      .catch(error => {
+//           console.error("getData error:" , error);
+//      });
+
+
+
+// // synchronous log
+// console.log("test");
