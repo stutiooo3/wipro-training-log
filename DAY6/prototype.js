90e705a4158f0a728230567264065da1cc13f04d
stuti <singhstuti003@gmail.com>
2026-05-07 17:23:54 +0530

    prototype

---
+++ DAY6/prototype.js
@@ -0,0 +1,13 @@
+function person(name) {
+    this.name = name;
+}
+ 
+person.prototype.sayHi = function() {
+    console.log('hello ' + this.name);
+}
+const me = new person("aryan");
+const you = new person("you");
+me.sayHi();
+you.sayHi();
+console.log(Object.getPrototypeOf(me) === person.prototype)
+console.log(me.sayHi === you.sayHi)
